3df2eb115f206af9ee7c929a5b55a65981dcfd8d
Moses-pro <72385897+Moses-pro@users.noreply.github.com>
2020-10-05 11:36:30 +0100

    Add files via upload

---
+++ camera website/script.js
@@ -0,0 +1,121 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+    
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+
+window.addEventListener('click', outsideClick);
+
+
+// function for outside
+function outsideClick(e){
+if(e.target == modal){
+    modal.style.display = 'none';
+}
+
+}
+// Get the modal
+var modal = document.getElementById("myModal-3");
+    
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg-2");
+var modalImg = document.getElementById("img01-2");
+var captionText = document.getElementById("caption-2");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+
+window.addEventListener('click', outsideClick);
+
+
+// function for outside
+function outsideClick(e){
+if(e.target == modal){
+    modal.style.display = 'none';
+}
+
+}
+// Get the modal
+var modal = document.getElementById("myModal-3");
+    
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg-3");
+var modalImg = document.getElementById("img01-3");
+var captionText = document.getElementById("caption-3");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+
+window.addEventListener('click', outsideClick);
+
+
+// function for outside
+function outsideClick(e){
+if(e.target == modal){
+    modal.style.display = 'none';
+}
+
+}
+// Get the modal
+var modal = document.getElementById("myModal-4");
+    
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg-4");
+var modalImg = document.getElementById("img01-4");
+var captionText = document.getElementById("caption-4");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+
+window.addEventListener('click', outsideClick);
+
+
+// function for outside
+function outsideClick(e){
+if(e.target == modal){
+    modal.style.display = 'none';
+}
+
+}
+
+// Get the modal
+var modal = document.getElementById("myModal-5");
+    
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg-5");
+var modalImg = document.getElementById("img01-5");
+var captionText = document.getElementById("caption-5");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+
+window.addEventListener('click', outsideClick);
+
+
+// function for outside
+function outsideClick(e){
+if(e.target == modal){
+    modal.style.display = 'none';
+}
+
+}
